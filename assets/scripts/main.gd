96531ecfca08de19ddae1d336cae51ecffffcebc
99p <poccori@gmail.com>
2023-03-21 22:55:41 +0900

    nice update

--- assets/scripts/main.gd
+++ assets/scripts/main.gd
@@ -1,45 +1,38 @@
-extends Control
+extends SafeMarginContainer
 
 var apiKey
+@export var system: Node
+@export var user: Node
+@export var asistant: Node
+@export var input: Node
+@export var setting_button: Node
 
 func _ready():
+	super()
+	input.grab_focus()
 	var f = FileAccess.open("user://api.dat", FileAccess.READ)
 	if not f: f.store_string("")
 	apiKey = f.get_as_text()
-	
-	# var os = OS.get_name()
-	# if os == "iOS":
-		# pass
-
-func _on_button_pressed():
-	# get_node("HTTPRequest").request("http://www.mocky.io/v2/5185415ba171ea3a00704eed")
-	gpt()
-
-func _on_http_request_request_completed(_result:int, _response_code:int, _headers:PackedStringArray, body:PackedByteArray):
-	var json = JSON.parse_string(body.get_string_from_utf8())
-	var message = json.choices[0].message.content
-	$MarginContainer/VBoxContainer/Asistant.text = message
 
 func _on_settings_pressed():
-	var s = get_node("MarginContainer/Setting")
+	var s = $View/Header/SettingButton/FontAwesome
 	var t = create_tween()
 	t.set_ease(Tween.EASE_IN_OUT)
 	t.set_trans(Tween.TRANS_ELASTIC)
-	t.tween_property(s, "rotation", PI*2, 1)
+	t.tween_property(s, "rotation", PI*2, 0.5)
 	t.tween_callback(func():
 						s.rotation = 0
 						get_tree().change_scene_to_file('res://assets/scenes/setting.tscn')
-						# get_tree().change_scene_to_packed(setting)
 						)
 
 func gpt():
 	var messages = [ {
 		'role': 'system',
-		'content': $MarginContainer/VBoxContainer/System.text
+		'content': system.text
 		},
 		{
 		'role': 'user',
-		'content': $MarginContainer/VBoxContainer/User.text
+		'content': user.text
 		},
 	]
 	var apiUrl = 'https://api.openai.com/v1/chat/completions'
@@ -61,3 +54,14 @@ func gpt():
 	
 	var o = JSON.stringify(options)
 	var _error = http_request.request(apiUrl, headers, HTTPClient.METHOD_POST, o)
+
+func _on_http_request_request_completed(_result:int, _response_code:int, _headers:PackedStringArray, body:PackedByteArray):
+	var json = JSON.parse_string(body.get_string_from_utf8())
+	var message = json.choices[0].message.content
+	asistant.text = message
+	print(json)
+
+func _on_user_text_submitted(_new_text:String):
+	user.text = input.text
+	input.text = ''
+	gpt()
--- assets/scripts/setting.gd
+++ assets/scripts/setting.gd
@@ -1,14 +1,19 @@
-extends Control
+extends SafeMarginContainer
 
 @export var main: PackedScene
 
 func _ready():
+	super()
 	var f = FileAccess.open("user://api.dat", FileAccess.READ)
 	if f:
-		$TextEdit.text = f.get_as_text()
+		$VBoxContainer/TextEdit .text = f.get_as_text()
 
 func _on_go_pressed():
 	var f = FileAccess.open("user://api.dat", FileAccess.WRITE)
-	f.store_string($TextEdit.text)
+	f.store_string($VBoxContainer/TextEdit.text)
 	get_tree().change_scene_to_packed(main)
 
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_packed(main)
